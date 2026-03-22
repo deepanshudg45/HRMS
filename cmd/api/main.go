a7ce2522667690ad1361c20c622e0192ad3ec077
Piyush4040 <141345027+Piyush4040@users.noreply.github.com>
2026-03-22 19:35:32 +0530

    utility

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-
+	"github.com/wil/hrms/pkg/db"
 	"github.com/wil/hrms/pkg/middleware"
 )
 
@@ -19,7 +19,23 @@ func main() {
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "ok"})
 	})
+	// db connection
+	r.GET("/api/v1/health", func(c *gin.Context) {
+	err := db.GetPool().Ping(c)
 
+	if err != nil {
+		c.JSON(500, gin.H{
+			"status": "DOWN",
+			"db":     "not reachable",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"status": "UP",
+		"db":     "connected",
+	})
+})
 	logger.Info("Server started on :8080")
 	r.Run(":8080")
 }
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -3,14 +3,32 @@ package db
 import (
 	"context"
 	"log"
+	"sync"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewDB(connStr string) *pgxpool.Pool {
-	db, err := pgxpool.New(context.Background(), connStr)
-	if err != nil {
-		log.Fatal("DB connection failed:", err)
-	}
-	return db
+var (
+	pool *pgxpool.Pool
+	once sync.Once
+)
+
+func InitDB(connStr string) {
+	once.Do(func() {
+		config, err := pgxpool.ParseConfig(connStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		config.MaxConns = 25
+
+		pool, err = pgxpool.NewWithConfig(context.Background(), config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+}
+
+func GetPool() *pgxpool.Pool {
+	return pool
 }
