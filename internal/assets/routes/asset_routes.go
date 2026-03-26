8054c925617774cf67bb88add73e17f02b44e574
PiyushRawat404 <piyush.rawat@marbleai.x>
2026-03-26 15:14:53 +0530

    feat:addded migration and defined routes properly

---
+++ internal/assets/routes/asset_routes.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+    "WITS/internal/assets/handler"
+
+    "github.com/gofiber/fiber/v2"
+)
+
+func AssetRoutes(app *fiber.App, handler *handler.AssetHandler) {
+    asset := app.Group("/api/v1")
+    //e2
+    asset.Post("/assets", handler.CreateAsset)
+    asset.Delete("/assets/:id", handler.DeleteAsset)
+    asset.Patch("/assets/:id/status", handler.UpdateAssetStatus)
+    asset.Get("/assets/my",handler.GetActiveAssets)
+    asset.Post("/assets/:id/return", handler.ReturnAsset)
+    asset.Patch("/assets/assignments/:id/acknowledge",handler.UpdateAssignment)
+    asset.Get("/assets/:id/assignments", handler.GetMyAssignments)
+    asset.Get("/assets/:id/maintenance", handler.GetMaintenanceRecords)
+    asset.Post("/assets/import", handler.ImportAssets)
+    asset.Get("/assets/admin/reports", handler.GenerateReport)
+    asset.Get("/assets/employee/:employeeId", handler.GetAssetsByEmployeeID)
+    //e1
+    asset.Get("/assets", handler.GetAssets)
+    asset.Get("/assets/:id", handler.GetAssetByID)
+    asset.Put("/assets/:id", handler.UpdateAsset)
+    asset.Post("/assets/:id/assign", handler.AssignAsset)
+    asset.Post("/assets/:id/maintenance", handler.CreateMaintenanceRecord)
+    asset.Patch("/assets/:id/maintenance/:maintenanceId", handler.UpdateMaintenanceRecord)
+}
